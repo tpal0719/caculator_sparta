bf3e551ca43bc746652482c23e2beeb5229734a5
semdev <tpal071997@gmail.com>
2024-04-25 17:57:48 +0900

    Level1- 7~8. 연산결과 무한히 저장/ inquiry입력시 연산결과 전부 출력

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -6,8 +7,12 @@ public class App {
     public static void main(String[] args) {
 
         //(5) 결과를 저장할 맵
-        int MaxIndex = 3;
-        String[] strResult = new String[MaxIndex];
+        //int MaxIndex = 3;
+        //String[] strResult = new String[MaxIndex];
+
+        // (7) 무한이 저장될 수 있도록
+        Queue<String> queueResult = new LinkedList<>();
+
         int count = 0; //주소값
 
         Scanner sc = new Scanner(System.in);
@@ -37,7 +42,11 @@ public class App {
                     if (num2 != 0) {
                         yield num1 / num2;
                     }
-                    yield 0;
+                    else{
+                        System.out.println("0으로 나눌수는 없습니다");
+                        yield 0;
+                    }
+                    
                 }
                 default -> {
                     System.out.println("잘못된 사칙연산");
@@ -51,32 +60,40 @@ public class App {
             String saveResult = num1 + " " + type + " " + num2 + " = " + result;
             // (6) 연산 결과가 10개를 초과하는 경우
             // 가장 먼저 저장된 결과를 삭제하고 새로운 연산 결과가 저장 (string Array로 직접해봄)
-            if (strResult[MaxIndex - 1] != null) {
-                for (int i = 0; i < MaxIndex - 1; i++) {
-                    strResult[i] = strResult[i + 1];
-                }
-                strResult[MaxIndex - 1] = saveResult;
-            } else {
-                strResult[count++] = saveResult;
-            }
+//            if (strResult[MaxIndex - 1] != null) {
+//                for (int i = 0; i < MaxIndex - 1; i++) {
+//                    strResult[i] = strResult[i + 1];
+//                }
+//                strResult[MaxIndex - 1] = saveResult;
+//            } else {
+//                strResult[count++] = saveResult;
+//            }
 
+            //(7) 연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정
+            queueResult.add(saveResult);
 
-            // (4) “exit” 문자열을 입력하기 전까지 무한으로 계산
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             sc.nextLine(); //입력오류 해결
+            // (8) remove 제일 먼저 연산된 결과 삭제
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String answer = sc.nextLine();
+            if(answer.equals("remove"))  queueResult.remove(); //삭제
 
-            if (answer.equals("exit")) {
-                //(6) 확인용
-                for (int i = 0; i < strResult.length; i++) {
-                    System.out.println(strResult[i]);
+            // (8) inquiry 저장된 연산 결과 출력
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            answer = sc.nextLine();
+            if(answer.equals("inquiry"))
+            {
+                Iterator iterator = queueResult.iterator();
+                while (iterator.hasNext())
+                {
+                    System.out.println(iterator.next()+ "\n");
                 }
-                break;
             }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            answer = sc.nextLine();
+            if (answer.equals("exit")) break;
 
         }
-
     }
-
-
 }
