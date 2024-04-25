4feb0ea6c00c257bff9adfaa56eafbde90672f52
semdev <tpal071997@gmail.com>
2024-04-25 16:24:39 +0900

    Level1-3.양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행/결과값을 출력

--- src/App.java
+++ src/App.java
@@ -10,5 +10,23 @@ public class App {
         int num2 = sc.nextInt();
         System.out.print("사칙연산 기호를 입력하세요: ");
         char type = sc.next().charAt(0);
+
+
+
+        double result = switch (type){
+            case '+' -> {yield num1 + num2;}
+            case '-' -> {yield num1 - num2;}
+            case '*' -> {yield num1 * num2;}
+            //“나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. “
+            case '/' -> {
+                if(num2!=0){
+                    yield num1 / num2;
+                }
+                yield 0;
+            }
+            default -> {System.out.println("잘못된 사칙연산"); yield 0;}
+        };
+        System.out.println(num1+" "+type+" "+num2);
+        System.out.println("결과: " + result);
     }
 }
