31928363df85a39fc8ea62b2380bcbd0771acaf3
semdev <tpal071997@gmail.com>
2024-04-26 10:29:20 +0900

    과제 링크 추가

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+//https://teamsparta.notion.site/Java-decda7a240094997a83d32154784e7cc
 public class App {
     public static void main(String[] args) {
 
